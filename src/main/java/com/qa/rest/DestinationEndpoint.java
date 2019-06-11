540188d9f7cf23545e26fa687e7d941b70893e22
dannylambord97 <dannylambord@hotmail.co.uk>
2019-06-11 21:13:16 +0100

    Added functionality to add destination

--- src/main/java/com/qa/rest/DestinationEndpoint.java
+++ src/main/java/com/qa/rest/DestinationEndpoint.java
@@ -1,17 +1,21 @@
 package com.qa.rest;
 
 
+import java.net.URI;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.Status;
 
 import com.qa.model.Destination;
@@ -56,6 +60,17 @@ public class DestinationEndpoint {
 		}
 		Destination destination = destRepo.read(id);
 		return Response.ok(destination).build();
+	}
+	
+	@POST
+	@Consumes({"application/json"})
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/destination")
+	public Response addAccount(Destination accountRS, @Context UriInfo uriInfo) {
+		accountRS = destRepo.create(accountRS);
+		URI createdURI = uriInfo.getBaseUriBuilder().path(""+accountRS.getId()).build();
+		System.out.println(createdURI);
+		return Response.ok(createdURI.toString()).status(Status.CREATED).build();
 	}
 	
 	
